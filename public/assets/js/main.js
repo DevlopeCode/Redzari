91456f9fd556ca457163db5f86485cc9061da39d
darkknight1599 <harshitchoursiya123@gmail.com>
2023-04-25 18:40:23 +0530

    Loder Close

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -761,29 +761,7 @@
     });
 
 
-    $(window).on('load', function () {
-
-        /*-----------------
-            preloader
-        ------------------*/
-        var preLoder = $("#preloader");
-        preLoder.fadeOut(0);
-
-        /*-----------------
-            back to top
-        ------------------*/
-        var backtoTop = $('.back-to-top')
-        backtoTop.fadeOut();
-
-        /*---------------------
-            Cancel Preloader
-        ----------------------*/
-        $(document).on('click', '.cancel-preloader a', function (e) {
-            e.preventDefault();
-            $("#preloader").fadeOut(2000);
-        });
-
-    });
+  
 	  $('.stone-go-top').find( 'a' ).on('click', function () {
 	       $(window).scrollTop(0);
 	    });
